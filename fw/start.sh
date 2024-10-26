92cc2b1c424511c311e8df9a5a054ae288ee88c1
horus <acasam01@estudiantes.unileon.es>
2024-10-26 17:00:48 +0200

    base config

---
+++ fw/start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Aseguro el bit de forwarding 
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+# Política para la cadena INPUT: Rechazar todo
+iptables -P INPUT DROP
+iptables -A INPUT -i lo -j ACCEPT 
+iptables -A INPUT -p icmp --icmp-type 8 -j ACCEPT 
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT 
+
+# Politica para la cadena OUTPUT: Permitir todo
+iptables -P OUTPUT ACCEPT
+
+# Política para la cadena FORWARD: Rechazar todo
+iptables -P FORWARD DROP
+
+# Permitir el paso de paquetes para conexiones abiertas o relacionadas
+iptables -A FORWARD -p tcp -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -p udp -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -p icmp -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+iptables -A FORWARD -i eth2 -s 10.5.2.0/24 -o eth1 -p tcp -j ACCEPT 
+iptables -A FORWARD -i eth2 -s 10.5.2.0/24 -o eth1 -p udp -j ACCEPT 
+iptables -A FORWARD -i eth2 -s 10.5.2.0/24 -o eth1 -p icmp -j ACCEPT
+
+# Iniciar servicio sshd
+/usr/sbin/sshd -D
---
+++ internal/int1/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Workarround para solventar el error que aparece si en la definici´on de la red ponemos como gateway
+# la IP de m´aquina que queremos que ejerza como tal (en este caso, fw). No podemos poner IPs
+# repetidas en el fichero docker-compose.yml
+route add default gw 10.5.2.1
+route del default gw 10.5.2.254
+
+# Cambiar nombre interefaz
+ip link set eth0 name int1
+
+# Iniciar servicio sshd
+/usr/sbin/sshd -D
