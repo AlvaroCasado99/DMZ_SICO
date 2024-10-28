9e1e97d0c1d70627a6176d56c286f5bf0ded6d08
horus <acasam01@estudiantes.unileon.es>
2024-10-28 20:40:00 +0100

    Falta arreglar la regla para prevenir DoS en FWç

--- fw/start.sh
+++ fw/start.sh
@@ -6,7 +6,7 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 # Política para la cadena INPUT: Rechazar todo
 iptables -P INPUT DROP
 iptables -A INPUT -i lo -j ACCEPT 
-iptables -A INPUT -p icmp --icmp-type 8 -j ACCEPT 
+#iptables -A INPUT -p icmp --icmp-type 8 -j ACCEPT 
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT 
 
 # Politica para la cadena OUTPUT: Permitir todo
@@ -27,6 +27,19 @@ iptables -A FORWARD -i eth2 -s 10.5.2.0/24 -o eth1 -p icmp -j ACCEPT
 
 # Configuro el fw para que enmascare las IPs de la red interna con la Ip de la gateway de la red externa
 iptables -t nat -A POSTROUTING -s 10.5.2.0/24 -o eth1 -j SNAT --to-source 10.5.0.1
+
+# --- Configuración de la política para la red DMZ
+# Accesos permitidos tcp desde la red interna
+iptables -A FORWARD -i eth2 -s 10.5.2.0/24 -o eth0 -p tcp --dport 80 -j ACCEPT
+
+# Accesos permitidos tcp desde la red externa ¿?
+iptables -A FORWARD -i eth1 -s 10.5.0.0/24 -o eth0 -p tcp --dport 80 -j ACCEPT
+
+# Privilegio de administrador para la maquina Int1 
+iptables -A FORWARD -i eth2 -s 10.5.2.20 -o eth0 -p tcp --dport 22 -j ACCEPT
+
+# Prevenir ataques DoS con ICMP desde la red DMZ al fw
+iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 5/minute --limit-burst 5 -j ACCEPT
 
 # Iniciar servicio sshd
 /usr/sbin/sshd -D
