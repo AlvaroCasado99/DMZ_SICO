ad4d5d79ab91af2c46c99248c7b8c726a1b9dd4f
horus <acasam01@estudiantes.unileon.es>
2024-10-29 12:03:45 +0100

    DoS prevention problem solved. ICMP packets are recived as RELATED not NEW

--- fw/start.sh
+++ fw/start.sh
@@ -6,8 +6,9 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 # Política para la cadena INPUT: Rechazar todo
 iptables -P INPUT DROP
 iptables -A INPUT -i lo -j ACCEPT 
-#iptables -A INPUT -p icmp --icmp-type 8 -j ACCEPT 
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT 
+# iptables -A INPUT -p icmp --icmp-type 8 -j ACCEPT 
+iptables -A INPUT -p tcp -m state --state ESTABLISHED,RELATED  -j ACCEPT 
+iptables -A INPUT -p udp -m state --state ESTABLISHED,RELATED -j ACCEPT 
 
 # Politica para la cadena OUTPUT: Permitir todo
 iptables -P OUTPUT ACCEPT
