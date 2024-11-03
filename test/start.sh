574d225223733155bf4556976c6aecefe846f8dd
horus <acasam01@estudiantes.unileon.es>
2024-11-03 01:56:58 +0100

    Muchas cosas. Supervisor. Con Supervisor y/o Google-auth no funciona fail2ban :)

--- test/start.sh
+++ test/start.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 # Initialize google-authenticator
-# google-authenticator -f -t -r 3 -R 30 -w 3 -l "docker@hostname" -s /root/.google_authenticator
+#google-authenticator -f -t -r 6 -R 60 -w 3 -l "SICO" -s /home/sico/.google_authenticator
 
 # Start rsyslog daemon
 rsyslogd
 
 # Start Fail2Ban service
-fail2ban-server -f &
+fail2ban-server -D &
 
 # Start SSH service
 /usr/sbin/sshd -D
+
+# Start supervisord
+# ---- Supervisor es incompatible con rsyslogd
+#/usr/bin/supervisord 
