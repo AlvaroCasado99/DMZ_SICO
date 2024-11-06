b3a76e553049ad3d7d78f5e00538b66b155f203b
horus <acasam01@estudiantes.unileon.es>
2024-11-06 01:48:33 +0100

    Fail2ban y authenticator funcionan juntos

--- test/start.sh
+++ test/start.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Initialize google-authenticator
-#google-authenticator -f -t -r 6 -R 60 -w 3 -l "SICO" -s /home/sico/.google_authenticator
-
 # Start rsyslog daemon
 rsyslogd
 
 # Start Fail2Ban service
-fail2ban-server -D &
+fail2ban-server -f &
 
 # Start SSH service
 /usr/sbin/sshd -D
